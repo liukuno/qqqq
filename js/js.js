65b0362bd12b40b8feba2556be67c872835985f9
liukuno <liukunoubar@163.com>
2016-03-26 10:14:43 +0800

    tregrt
    rgfgege

---
+++ js/js.js
@@ -0,0 +1,15 @@
+;(function(){
+
+$(function(){
+	$("#yuan").on("tap",function(){
+		
+		$("#dong").css({
+			 "-webkit-transform":"translateX(200px)",
+                "-webkit-transition":"all 2s"
+		})
+
+	})
+
+
+})
+})(Zepto)
